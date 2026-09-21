b25b5153bbb7e897f74ac561b6d700c89ca6be9a
Pseki77 <parth07681@gmail.com>
2026-09-21 15:35:04 +0530

    polishing

--- Settings preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('settingsApi', {
-  get: () => ipcRenderer.invoke('settings:get'),
-  update: (change) => ipcRenderer.invoke('settings:update', change),
-  restart: () => ipcRenderer.invoke('settings:restart'),
-});
